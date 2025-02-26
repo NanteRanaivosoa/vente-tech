26ac3d478c02321fdaa80cab9f903b569ea266c7
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-02-26 11:50:32 +0100

    Style Catalog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles.css";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
       </Routes>
     </BrowserRouter>
 
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import productsData from "../data/products.json"; 
+import "./Catalog.css"
 
 
 const Catalog = () => {
-
-    const [products, _] = useState();
-
+    const [products, _] = useState(productsData);
+  
     return (
-        <div className="catalog">
-            <h2>Catalogue des Produits</h2>
-            <div className="product-list">
-                {products.map((product) => (
-                    <div key={product.id} className="product-card">
-                        <img src="{product.image}" alt="{product.title}" />
-                        <h3>{product.title}</h3>
-                        <p>{product.price} Ariary</p>
-                        <Link to={`/product/${product.id}`} className="details-btn">
-                            Voir details
-                        </Link>
-                    </div>
-                ))}
-
+      <div className="catalog">
+        <h2>Catalogue des Produits</h2>
+        <div className="product-list">
+          {products.map((product) => (
+            <div key={product.id} className="product-card">
+              <img src={product.image} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>{product.price} Ar</p>
+              <Link to={`/product/${product.id}`} className="details-btn">
+                Voir Détails
+              </Link>
             </div>
+          ))}
         </div>
-
+      </div>
     );
-};
-export default Catalog;
+  };
+  
+  export default Catalog;
